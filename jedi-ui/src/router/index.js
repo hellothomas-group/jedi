c3cc45a08da71f296312a36c803074a430abb768
hellothomas <tyty2017@qq.com>
2021-06-30 14:57:13 +0800

    redirect to login

--- jedi-ui/src/router/index.js
+++ jedi-ui/src/router/index.js
@@ -38,6 +38,10 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
       name: 'Login',
       component: Login
     },
