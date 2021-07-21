6f14d2bf7fa4216f9d8cf3aaf07337ba102595a1
hellothomas <tyty2017@qq.com>
2021-07-21 10:49:04 +0800

    optimize delta.sql

--- doc/db/delta.sql
+++ doc/db/delta.sql
@@ -1,5 +1,5 @@
 ALTER TABLE config.release_history CHANGE `Id` id int unsigned auto_increment NOT NULL COMMENT '自增Id';
 ALTER TABLE config.audit CHANGE `Id` id int unsigned auto_increment NOT NULL COMMENT '主键';
 
-ALTER TABLE monitor.release_history MODIFY COLUMN operation_context VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发布上下文信息';
-ALTER TABLE monitor.`release` MODIFY COLUMN configurations varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发布配置';
+ALTER TABLE config.release_history MODIFY COLUMN operation_context VARCHAR(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发布上下文信息';
+ALTER TABLE config.`release` MODIFY COLUMN configurations varchar(4096) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '发布配置';
