4a98b3bee8e10422dc109f70e12bea8c391fd6a3
hellothomas <tyty2017@qq.com>
2021-11-09 23:26:59 +0800

    sync

--- jedi-admin/src/main/java/xyz/hellothomas/jedi/admin/api/AppController.java
+++ jedi-admin/src/main/java/xyz/hellothomas/jedi/admin/api/AppController.java
@@ -1,15 +1,19 @@
 package xyz.hellothomas.jedi.admin.api;
 
 import io.swagger.annotations.Api;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 import xyz.hellothomas.jedi.admin.api.dto.AppRequest;
 import xyz.hellothomas.jedi.admin.api.dto.AppResponse;
 import xyz.hellothomas.jedi.admin.api.dto.PageHelperRequest;
 import xyz.hellothomas.jedi.admin.api.dto.PageResult;
 import xyz.hellothomas.jedi.admin.application.AppService;
-import xyz.hellothomas.jedi.biz.domain.monitor.App;
 import xyz.hellothomas.jedi.admin.infrastructure.annotation.UserLoginToken;
+import xyz.hellothomas.jedi.admin.infrastructure.listener.SyncEvent;
+import xyz.hellothomas.jedi.biz.common.enums.SyncOperationEnum;
+import xyz.hellothomas.jedi.biz.common.enums.SyncTypeEnum;
 import xyz.hellothomas.jedi.biz.common.utils.LocalBeanUtils;
+import xyz.hellothomas.jedi.biz.domain.monitor.App;
 import xyz.hellothomas.jedi.biz.infrastructure.exception.BadRequestException;
 import xyz.hellothomas.jedi.core.dto.ApiResponse;
 
@@ -29,15 +33,17 @@ import static xyz.hellothomas.jedi.admin.common.utils.JwtUtil.CLAIM_USER_NAME;
 public class AppController {
 
     private final AppService appService;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
-    public AppController(AppService appService) {
+    public AppController(AppService appService, ApplicationEventPublisher applicationEventPublisher) {
         this.appService = appService;
+        this.applicationEventPublisher = applicationEventPublisher;
     }
 
     @PostMapping("/namespaces/{namespaceName}/apps/{appId}")
     public ApiResponse<AppResponse> create(@PathVariable("namespaceName") String namespaceName,
-                                          @Valid @RequestBody AppRequest appRequest,
-                                          @RequestAttribute(CLAIM_USER_NAME) String operator) {
+                                           @Valid @RequestBody AppRequest appRequest,
+                                           @RequestAttribute(CLAIM_USER_NAME) String operator) {
 
         App entity = LocalBeanUtils.transform(App.class, appRequest);
         App managedEntity = appService.findOne(entity.getNamespaceName(), entity.getAppId());
@@ -48,28 +54,32 @@ public class AppController {
 
         entity = appService.save(entity, operator);
 
+        applicationEventPublisher.publishEvent(new SyncEvent(entity, SyncTypeEnum.APP, SyncOperationEnum.CREATION));
+
         return ApiResponse.success(LocalBeanUtils.transform(AppResponse.class, entity));
     }
 
     @PutMapping("/namespaces/{namespaceName}/apps/{appId}")
     public ApiResponse<String> update(@PathVariable String namespaceName, @PathVariable String appId,
-                       @RequestBody AppRequest request,
-                       @RequestAttribute(CLAIM_USER_NAME) String operator) {
+                                      @RequestBody AppRequest request,
+                                      @RequestAttribute(CLAIM_USER_NAME) String operator) {
         if (!Objects.equals(namespaceName, request.getNamespaceName())) {
             throw new BadRequestException("The namespace name of path variable and request body is different");
         }
 
         App entity = LocalBeanUtils.transform(App.class, request);
 
-        appService.update(entity, operator);
+        entity = appService.update(entity, operator);
+
+        applicationEventPublisher.publishEvent(new SyncEvent(entity, SyncTypeEnum.APP, SyncOperationEnum.CHANGE));
 
         return ApiResponse.success("更新成功");
     }
 
     @DeleteMapping("/namespaces/{namespaceName}/apps/{appId}")
     public ApiResponse<String> delete(@PathVariable("namespaceName") String namespaceName,
-                                @PathVariable("appId") String appId,
-                       @RequestAttribute(CLAIM_USER_NAME) String operator) {
+                                      @PathVariable("appId") String appId,
+                                      @RequestAttribute(CLAIM_USER_NAME) String operator) {
         App entity = appService.findOne(namespaceName, appId);
         if (entity == null) {
             throw new BadRequestException("namespace app not found for namespace: " + namespaceName + " " +
@@ -77,12 +87,14 @@ public class AppController {
         }
         appService.deleteApp(entity, operator);
 
+        applicationEventPublisher.publishEvent(new SyncEvent(entity, SyncTypeEnum.APP, SyncOperationEnum.DELETION));
+
         return ApiResponse.success("删除成功");
     }
 
     @GetMapping("/namespaces/{namespaceName}/apps")
     public ApiResponse<PageResult<AppResponse>> getAppNamespaces(@PathVariable("namespaceName") String namespaceName,
-                                                    PageHelperRequest pageHelperRequest) {
+                                                                 PageHelperRequest pageHelperRequest) {
         PageResult<App> appsPage = appService.findByNamespaceName(namespaceName, pageHelperRequest);
 
         return ApiResponse.success(transform2PageResult(appsPage));
@@ -90,7 +102,7 @@ public class AppController {
 
     @GetMapping("/namespaces/{namespaceName}/apps/{appId}")
     public ApiResponse<AppResponse> getAppNamespaces(@PathVariable("namespaceName") String namespaceName,
-                                        @PathVariable("appId") String appId) {
+                                                     @PathVariable("appId") String appId) {
         App app = appService.findOne(namespaceName, appId);
 
         return ApiResponse.success(LocalBeanUtils.transform(AppResponse.class, app));
